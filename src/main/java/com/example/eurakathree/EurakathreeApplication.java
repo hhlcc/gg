e710f30278dc6f6b821e7ed176882977be80c51e
test111 <383561108@qq.com>
2021-05-27 17:10:16 +0800

    测

---
+++ src/main/java/com/example/eurakathree/EurakathreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurakathree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakathreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakathreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eurakathree/EurakathreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurakathree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakathreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
